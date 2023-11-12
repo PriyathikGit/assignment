87391462a3378bef0ba1684737688879d457b609
Rohan <techhack237@gmail.com>
2023-11-12 13:35:30 +0530

    new changes

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ const rowBox = document.getElementById('rowBox')
 btn.addEventListener('click',()=>{
     window.location.href="index2.html"
 })
-
+document.getElementById('newBtn').addEventListener('click',()=>{
+    window.location.href="index2.html"
+})
 // leftIcons.forEach(function(icon) {
 //     icon.style.display = "block";
 // });
@@ -29,26 +31,71 @@ rightBtn.addEventListener('click',()=>{
 
 let isLeftBarExpanded = false;
 
+
+// ham.addEventListener('click', () => {
+//     if (isLeftBarExpanded) {
+//         leftBar.style.width = "70px";
+//         leftIcons.forEach(function (icon) {
+//             icon.style.display = "flex";
+//         });
+//         rowBox.style.width="10rem";
+//         rowBox.style.display="flex";
+//         rowBox.style.justifyContent="space-between";
+//     } else {
+//         leftBar.style.display="flex"
+//         leftBar.style.width = "200px";
+//         leftIcons.forEach(function (icon) {
+//             icon.style.display = "none";
+//         });
+//         rowBox.style.display="none"
+//     }
+
+//     isLeftBarExpanded = !isLeftBarExpanded; // Toggle the state
+// });
+
 ham.addEventListener('click', () => {
     if (isLeftBarExpanded) {
+        // Code for when left bar is expanded
         leftBar.style.width = "70px";
         leftIcons.forEach(function (icon) {
-            icon.style.display = "block";
+            icon.style.display = "flex";
         });
-        rowBox.style.width="10rem";
-        rowBox.style.display="flex";
-        rowBox.style.justifyContent="space-between";
+        rowBox.style.width = "10rem";
+        rowBox.style.display = "flex";
+        rowBox.style.justifyContent = "space-between";
     } else {
-        leftBar.style.display="block"
+        // Code for when left bar is not expanded
+        leftBar.style.display = "flex"
         leftBar.style.width = "200px";
         leftIcons.forEach(function (icon) {
             icon.style.display = "none";
         });
-        rowBox.style.display="none"
+        rowBox.style.display = "none"
     }
 
     isLeftBarExpanded = !isLeftBarExpanded; // Toggle the state
 });
+
+// Add an event listener to reset the state when the document is clicked
+document.addEventListener('click', (event) => {
+    const target = event.target;
+    const isHamButton = target === ham || ham.contains(target);
+
+    // Check if the clicked element is not the ham button or its children
+    if (!isHamButton) {
+        // Reset to default state
+        leftBar.style.width = ""; // Set to default width
+        leftIcons.forEach(function (icon) {
+            icon.style.display = ""; // Set to default display
+        });
+        rowBox.style.width = ""; // Set to default width
+        rowBox.style.display = ""; // Set to default display
+        rowBox.style.justifyContent = ""; // Set to default justify content
+
+        isLeftBarExpanded = false; // Set the state to default
+    }
+});
+
 
 
 leftBar.addEventListener('mouseover',(e)=>{
